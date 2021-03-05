f7876c764b20f3e8c6a9fba07b67372439570fb4
yhcho0405 <yhcho0405@kakao.com>
2021-03-05 21:47:49 +0900

    rename

---
+++ _libft/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youncho <youncho@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/31 11:40:08 by youncho           #+#    #+#             */
+/*   Updated: 2021/03/05 21:45:41 by youncho          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void *ret;
+
+	if (!(ret = malloc(count * size)))
+		return (0);
+	ft_bzero(ret, count * size);
+	return (ret);
+}
